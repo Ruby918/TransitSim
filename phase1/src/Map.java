c41f8276c0c022ba24308ef803dd9b2e395d8dff
Loic Nassif <loic.nassif.lachapelle@mail.utoronto.ca>
2018-07-06 16:47:06 -0400

    minor changes to Map and SubwayStation to reflect the example usage

--- phase1/src/Map.java
+++ phase1/src/Map.java
@@ -1,11 +1,16 @@
 /* Loic */
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Map {
-
   private ArrayList<Route> routes;
+
   public Map(ArrayList<Route> routes) {
     this.routes = routes;
+  }
+
+  public void makeAdjacent(Station[2] stations) {
+    stations[0].addAdjacentStation(stations[1]);
+    stations[1].addAdjacentStation(stations[0]);
   }
 }
--- phase1/src/SubwayStation.java
+++ phase1/src/SubwayStation.java
@@ -7,4 +7,12 @@ public class SubwayStation extends Station {
   public SubwayStation(String name, Route route) {
     super(name, 0, 0.5, route);
   }
+
+  public void getTapInPrice() {
+    return this.tapInPrice;
+  }
+
+  public void getPassThroughPrice() {
+    return this.passThroughPrice;
+  }
 }
