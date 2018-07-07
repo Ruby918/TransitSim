2d4595414f468c6497029e2f6f2e5cb93370c35e
quachbr3 <briann.quach@mail.utoronto.ca>
2018-07-07 14:45:33 -0400

    Brian's changes to trip, tap classes

--- phase1/src/Trip.java
+++ phase1/src/Trip.java
@@ -1,10 +1,13 @@
 /* Brian */
 
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Trip {
-
+  private final double MAX_CHARGE = 6;
+  private double costSoFar;
   private TapInEvent startEvent;
   private TapEvent lastTapAdded;
   private ArrayList<TapEvent> tapInEvents = new ArrayList<>();
@@ -25,6 +28,17 @@ public class Trip {
     // given the start time and location of the last tap
     // if it isn't, throw error
     // return the price of this tap
+    if (!isTapInEventValid(tapInEvent)) {
+      throw new TripInvalidTapEventException();
+    } else if (tapInEvent.getStation() instanceof BusStation) {
+      double maxChargeAmount = MAX_CHARGE - costSoFar;
+      double chargeAmount;
+      if (maxChargeAmount > tapInEvent.getStation().tapInPrice) {
+        chargeAmount = tapInEvent.getStation().tapInPrice;
+      } else chargeAmount = maxChargeAmount;
+      costSoFar += chargeAmount;
+      return chargeAmount;
+    }
     return 0;
   }
 
@@ -33,6 +47,21 @@ public class Trip {
     // given the start time and location of the last tap
     // if it isn't, throw error
     // return the price of this tap
+    // if tapOutEvent is past 2hrs. from first tapInEvent 2hrs = 2*60*60*1000 ms.
+    // isTapInEventInSameTrip method
+    if (!isTapOutEventValid(tapOutEvent)) {
+      throw new TripInvalidTapEventException();
+    } else if (tapOutEvent.getStation() instanceof SubwayStation) {
+      //if last tap wasn't also a subway station tap
+      if (!(lastTapAdded.getStation() instanceof SubwayStation)){
+        throw new TripInvalidTapEventException();
+      }
+      else{
+        int distance = tapOutEvent.getStation().
+
+      }
+    }
+
     return 0;
   }
 
@@ -52,11 +81,7 @@ public class Trip {
   }
 
   private boolean isTapOutEventValid(TapOutEvent tapOutEvent) {
-      // if tapOutEvent is past 2hrs. from first tapInEvent 2hrs = 2*60*60*1000 ms. isTapInEventInSameTrip method
-      if (tapOutEvent.getDate().getTime() - startEvent.getDate().getTime() > 7200000) {
-          return false;
-      }
-      // 2 tapOutEvents in a row
+    // 2 tapOutEvents in a row
     if (lastTapAdded instanceof TapOutEvent) {
       return false;
     }
