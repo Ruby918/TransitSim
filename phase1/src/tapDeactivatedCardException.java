d4d4488d3f52630fdc14675c61e8aca2f34e27a4
Dhanraj Patel <dhanraj@live.ca>
2018-07-08 18:35:00 -0400

    Created tapDeactivatedCardException class in case a card that is deactivated is tapped.

---
+++ phase1/src/tapDeactivatedCardException.java
@@ -0,0 +1,7 @@
+public class tapDeactivatedCardException extends Exception{
+    public tapDeactivatedCardException(){
+        super("A card that was illegally was deactivated was tapped.");
+    }
+}
+
+
