0698ff4fe0dbacbb07fc47faa355b543a8533a7f
Danya Lette <danyalette@gmail.com>
2018-07-09 15:30:45 -0400

    bugfix: station

--- phase1/src/Station.java
+++ phase1/src/Station.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
 
 abstract public class Station {
 
-  protected static final double TAP_IN_PRICE;
-  protected static final double PASS_THROUGH_PRICE;
-  protected static final String NAME;
-  protected static final Route ROUTE;
-  protected static final ArrayList<Station> ADJACENT_STATIONS = new ArrayList<>();
+  protected final double TAP_IN_PRICE;
+  protected final double PASS_THROUGH_PRICE;
+  protected String name;
+  protected Route route;
+  protected ArrayList<Station> adjacentStations = new ArrayList<>();
 
   protected static String GENERIC_IDENTIFIER;
 
   public Station(String name, double tapInPrice, double passThroughPrice, Route route){
     this.TAP_IN_PRICE = tapInPrice;
     this.PASS_THROUGH_PRICE = passThroughPrice;
-    this.NAME = name;
-    this.ROUTE = route;
+    this.name = name;
+    this.route = route;
   }
 
   public void addAdjacentStation(Station station) {
-    this.ADJACENT_STATIONS.add(station);
+    this.adjacentStations.add(station);
   }
 
   public boolean isAdjacentToStation(Station station) {
-    return this.ADJACENT_STATIONS.contains(station);
+    return this.adjacentStations.contains(station);
   }
 
   public Route getRoute() {
-    return this.ROUTE;
+    return this.route;
+  }
+  public String getName() {
+    return this.name;
   }
 
 }
