4f34434fafa4880de91fe0711e6855808d7d7d96
Danya Lette <danyalette@gmail.com>
2018-07-09 16:32:15 -0400

    remove unused code in SubwayStation

--- phase1/src/SubwayStation.java
+++ phase1/src/SubwayStation.java
@@ -7,12 +7,4 @@ public class SubwayStation extends Station {
   public SubwayStation(String name, Route route) {
     super(name, 0, 0.5, route);
   }
-
-  public double getTapInPrice() {
-    return this.tapInPrice;
-  }
-
-  public double getPassThroughPrice() {
-    return this.passThroughPrice;
-  }
 }
