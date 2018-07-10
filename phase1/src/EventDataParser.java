d7f4aa74527f6581d7a0214f024cb846e1df43f6
Danya Lette <danyalette@gmail.com>
2018-07-10 00:28:47 -0400

    add basic event data parsing

--- phase1/src/EventDataParser.java
+++ phase1/src/EventDataParser.java
@@ -1,10 +1,154 @@
 public class EventDataParser extends DataParser {
 
-  public EventDataParser(String filename) {
+  private String message;
+  private TransitFareManager ttc;
+
+  public EventDataParser(String filename, TransitFareManager ttc) {
     super(filename);
+    this.ttc = ttc;
   }
 
   protected void parseLine(String line){
+
+    // Ignore empty line
+    if (line.isEmpty()) return;
+
+    // Reset message which will be printed after parsing line
+    this.message = "";
+
+    // Print input message
+    System.out.println("INPUT    :   " + line);
+
+    // Parse according to command type
+    String[] lineData = line.split(": ");
+    switch (lineData[0]) {
+      case "Admin": parseAdminCommand(lineData); break;
+      case "Customer": parseCustomerCommand(lineData); break;
+      case "Card": parseCardCommand(lineData); break;
+      default: this.message = "That is not a valid command type.";
+    }
+
+    // Print output message
+    System.out.println("OUTPUT   :   " + this.message + System.lineSeparator());
+  }
+
+  private void parseAdminCommand(String[] data) {
+    switch (data[1]) {
+      case "Revenue": parseAdminRevenueCommand(data); break;
+      case "Trips": parseAdminTripsCommand(data); break;
+      case "Stations": parseAdminStationsCommand(data); break;
+      case "Routes": parseAdminRoutesCommand(data); break;
+      case "Customers": parseAdminCustomersCommand(data); break;
+      case "Cards": parseAdminCardsCommand(data); break;
+      case "Report": parseAdminReportCommand(data); break;
+      default:  this.message = "That is not a valid admin command.";
+    }
+  }
+
+  private void parseCustomerCommand(String[] data) {
+
+    CustomerAccount customer = ttc.getCustomerById(Integer.parseInt(data[1]));
+    if (customer == null) {
+      this.message = "That customer does not exist.";
+      return;
+    }
+
+    String[] parameters = data[2].split(", ");
+
+    switch (parameters[0]) {
+      case "Report": this.message = customer.toString(); break;
+      case "Update Name":
+        customer.setName(parameters[1]);
+        this.message = "Successfully updated customer's name to " + parameters[1];
+        break;
+      case "Cards": this.message = customer.listCards(); break;
+      case "Average Cost": this.message = customer.getAverageMonthlyCost(); break;
+      case "Recent Trips":
+        this.message = "Recent Trips:" + System.lineSeparator() + customer.listRecentTrips();
+        break;
+      default:  this.message = "That is not a valid customer command.";
+    }
+  }
+
+  private void parseCardCommand(String[] data) {
+
+    Card card = ttc.getCardById(Integer.parseInt(data[1]));
+    if (card == null) {
+      this.message = "That card does not exist.";
+      return;
+    }
+
+    String[] parameters = data[2].split(", ");
+
+    switch (parameters[0]) {
+      case "Report": this.message = card.toString(); break;
+      case "Add Funds":
+        switch(parameters[1]) {
+          case "10":
+            card.addTenDollars();
+            this.message = "Successfully added $10 to card.";
+            break;
+          case "20":
+            card.addTwentyDollars();
+            this.message = "Successfully added $20 to card.";
+            break;
+          case "50":
+            card.addFiftyDollars();
+            this.message = "Successfully added $50 to card.";
+            break;
+          default: this.message = "That is not a valid amount.";
+        }
+        break;
+      case "Balance":
+        this.message = "$" + String.valueOf(card.getBalance());
+        break;
+      case "Deactivate":
+        card.deactivate();
+        this.message = "Successfully deactivated card.";
+        break;
+      case "Tap In":
+        parseCardTapIn(card, parameters);
+        break;
+      case "Tap Out":
+        parseCardTapOut(card, parameters);
+        break;
+      default:  this.message = "That is not a valid card command.";
+    }
+  }
+
+  private void parseAdminRevenueCommand(String[] data) {
+
+  }
+
+  private void parseAdminTripsCommand(String[] data) {
+
+  }
+
+  private void parseAdminStationsCommand(String[] data) {
+
+  }
+
+  private void parseAdminRoutesCommand(String[] data) {
+
+  }
+
+  private void parseAdminCustomersCommand(String[] data) {
+
+  }
+
+  private void parseAdminCardsCommand(String[] data) {
+
+  }
+
+  private void parseAdminReportCommand(String[] data) {
+
+  }
+
+  private void parseCardTapIn(Card card, String[] parameters) {
+
+  }
+
+  private void parseCardTapOut(Card card, String[] parameters) {
 
   }
 
--- phase1/src/TransitFareManager.java
+++ phase1/src/TransitFareManager.java
@@ -14,13 +14,17 @@ public class TransitFareManager {
   }
 
   public static void main(String[] args) {
-
+    // Create map
     Map thisMap = new Map();
     MapDataParser mapData = new MapDataParser("map.txt", thisMap);
     mapData.parse();
 
+    // Create transit fare manager
     TransitFareManager ttc = new TransitFareManager(thisMap);
 
+    // Process events from events.txt
+    EventDataParser eventData = new EventDataParser("events.txt", ttc);
+    eventData.parse();
   }
 
   public String getDailyReport(Date date) {
@@ -29,6 +33,7 @@ public class TransitFareManager {
   }
 
   public CustomerAccount createCustomerAccount(String name, String email) {
+    // Increment customer id by one for every new customer
     CustomerAccount customer = new CustomerAccount(name, email, this.customers.size());
     this.customers.add(customer);
     return customer;
@@ -38,11 +43,16 @@ public class TransitFareManager {
     return this.customers.get(id);
   }
 
+  public Card getCardById(int id) {
+    return this.cards.get(id);
+  }
+
   public void transferCardToCustomer(Card card, CustomerAccount customer) {
     customer.addCard(card);
   }
 
   public Card generateCard(CustomerAccount customer) {
+    // Increment card id by one for every new card
     Card card = new Card(this.cards.size());
     this.cards.add(card);
     return card;
