21adb7ace4cb021fbc0df898b7ac683229e17a5a
Danya Lette <danyalette@gmail.com>
2018-07-09 23:16:38 -0400

    add customer and card creation methods to TransitFareManager

--- phase1/src/TransitFareManager.java
+++ phase1/src/TransitFareManager.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class TransitFareManager {
 
   private ArrayList<CustomerAccount> customers = new ArrayList<>();
+  private ArrayList<Card> cards = new ArrayList<>();
   private Map map;
 
   public TransitFareManager(Map map) {
@@ -15,9 +16,11 @@ public class TransitFareManager {
   public static void main(String[] args) {
 
     Map thisMap = new Map();
-    TransitFareManager ttc = new TransitFareManager(thisMap);
     MapDataParser mapData = new MapDataParser("map.txt", thisMap);
     mapData.parse();
+
+    TransitFareManager ttc = new TransitFareManager(thisMap);
+
   }
 
   public String getDailyReport(Date date) {
@@ -25,9 +28,24 @@ public class TransitFareManager {
     return "";
   }
 
+  public CustomerAccount createCustomerAccount(String name, String email) {
+    CustomerAccount customer = new CustomerAccount(name, email, this.customers.size());
+    this.customers.add(customer);
+    return customer;
+  }
+
+  public CustomerAccount getCustomerById(int id) {
+    return this.customers.get(id);
+  }
+
+  public void transferCardToCustomer(Card card, CustomerAccount customer) {
+    customer.addCard(card);
+  }
+
   public Card generateCard(CustomerAccount customer) {
-    // stub
-    return new Card();
+    Card card = new Card(this.cards.size());
+    this.cards.add(card);
+    return card;
   }
 
 }
