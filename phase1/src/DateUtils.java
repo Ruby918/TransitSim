15b68d3207ff35ddf05ca1ad26142250fd958127
Danya Lette <danyalette@gmail.com>
2018-07-10 14:30:33 -0400

    implement DateUtils.datesOnSameDay method

--- phase1/src/DateUtils.java
+++ phase1/src/DateUtils.java
@@ -36,8 +36,17 @@ public class DateUtils {
     return calendar.getTime();
   }
 
+
   public static boolean datesOnSameDay(Date date1, Date date2) {
-    // stub
-    return false;
-  }
+
+    // Code adapted from https://stackoverflow.com/a/2517824/3200577
+    // (User: Michael Borgwardt)
+
+    Calendar cal1 = Calendar.getInstance();
+    Calendar cal2 = Calendar.getInstance();
+    cal1.setTime(date1);
+    cal2.setTime(date2);
+    return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+        cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+    }
 }
