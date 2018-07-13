4a21b705f1f442286a0d072c77151df586e3e0ab
Danya Lette <danyalette@gmail.com>
2018-07-13 10:38:40 -0400

    add test directory with sample test

---
+++ phase2/test/TestTapEvent.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTapEvent {
+
+  @Test
+  void TestTapInEventStation() {
+
+    Route sr = new SubwayRoute("SR");
+    Station st = new SubwayStation("ST", sr);
+
+    TapInEvent te = new TapInEvent(st, TransitDate.createFromDateString("10/10/2010"));
+    assertEquals(te.getStation(), st);
+  }
+
+}
