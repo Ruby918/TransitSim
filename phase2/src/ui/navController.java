6cd13c5199c487398ba0a0c0fe1a3be719b0e29c
Danya Lette <danyalette@gmail.com>
2018-08-03 10:11:41 -0400

    add login and logout button to nav

--- phase2/src/ui/navController.java
+++ phase2/src/ui/navController.java
@@ -1,16 +1,72 @@
 package ui;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
-public class navController extends UiController {
+public class NavController extends UiController {
+
+  @FXML
+  private Button loginButton;
+
+  @FXML
+  private Button logoutButton;
 
   @FXML
   public Label loggedInLabel;
+
   @FXML
   public void initialize() {
     if (user != null) {
       loggedInLabel.setText("Logged in as " + user.getName());
+      logoutButton.setVisible(true);
+      loginButton.setVisible(false);
+    }
+    else {
+      loggedInLabel.setText("");
+      logoutButton.setVisible(false);
+      loginButton.setVisible(true);
+    }
+  }
+
+  @FXML
+  protected void handleLogoutButtonAction(ActionEvent event) {
+    Window owner = loginButton.getScene().getWindow();
+    user = null;
+
+    try {
+      FXMLLoader titleLoader = new FXMLLoader();
+      titleLoader.setLocation(getClass().getResource("template/title_screen.fxml"));
+      Scene loginScene = new Scene(titleLoader.load(), 500, 500);
+      Stage loginStage = new Stage();
+      loginStage.setScene(loginScene);
+      owner.hide();
+      loginStage.show();
+    } catch (IOException e) {
+      logger.error("Failed to load title screen from nav.");
+    }
+  }
+
+  @FXML
+  protected void handleLoginButtonAction(ActionEvent event) {
+    Window owner = loginButton.getScene().getWindow();
+
+    try {
+      FXMLLoader loginLoader = new FXMLLoader();
+      loginLoader.setLocation(getClass().getResource("template/user_login_screen.fxml"));
+      Scene loginScene = new Scene(loginLoader.load(), 500, 500);
+      Stage loginStage = new Stage();
+      loginStage.setScene(loginScene);
+      owner.hide();
+      loginStage.show();
+    } catch (IOException e) {
+      logger.error("Failed to load login screen from nav.");
     }
   }
 }
