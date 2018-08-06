d5e598a08de18b814352ebc55fd95334c3156d61
Danya Lette <danyalette@gmail.com>
2018-08-06 15:08:25 -0400

    add loadTemplate to UiController

--- phase2/src/ui/NavigationController.java
+++ phase2/src/ui/NavigationController.java
@@ -1,18 +1,13 @@
 package ui;
 
-import java.io.IOException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Menu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
-import javafx.stage.Window;
 import transit.UserAccount;
 
 public class NavigationController extends UiController implements Initializable {
@@ -90,18 +85,6 @@ public class NavigationController extends UiController implements Initializable 
   }
 
   private void loadTemplate(String template) {
-    Window owner = menuBar.getScene().getWindow();
-
-    try {
-      FXMLLoader titleLoader = new FXMLLoader();
-      titleLoader.setLocation(getClass().getResource(template));
-      Scene loginScene = new Scene(titleLoader.load(), 300, 250);
-      Stage loginStage = new Stage();
-      loginStage.setScene(loginScene);
-      owner.hide();
-      loginStage.show();
-    } catch (IOException e) {
-      logger.log.severe("Failed to load " + template + " from nav.");
-    }
+    super.loadTemplate(template, menuBar);
   }
 }
--- phase2/src/ui/UiController.java
+++ phase2/src/ui/UiController.java
@@ -1,6 +1,12 @@
 package ui;
 
 import api.Api;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import transit.TransitLogger;
 import transit.Station;
 import transit.PriceModifier;
@@ -11,4 +17,20 @@ public class UiController {
 
   //UI runtime data store
   public static UiDataStore dataStore = new UiDataStore();
+
+  protected void loadTemplate(String template, Control control) {
+    Window owner = control.getScene().getWindow();
+
+    try {
+      FXMLLoader titleLoader = new FXMLLoader();
+      titleLoader.setLocation(getClass().getResource(template));
+      Scene loginScene = new Scene(titleLoader.load(), 600, 600);
+      Stage loginStage = new Stage();
+      loginStage.setScene(loginScene);
+      owner.hide();
+      loginStage.show();
+    } catch (IOException e) {
+      logger.log.severe("Failed to load " + template);
+    }
+  }
 }
